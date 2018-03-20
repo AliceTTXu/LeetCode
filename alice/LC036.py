2435375fa0c2d59d3c5981826129150ea58b6a6e
alice <aliceadelice@gmail.com>
2018-03-19 22:54:41 -0700

    valid sudoku: go through all elements, mark number elements as (rowIndex, num),

---
+++ alice/LC036.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        
+        for x in board:
+            temp = [num for num in x if num != '.']
+            if len(set(temp)) != len(temp):
+                return False
+
+        for x in zip(*board):
+            temp = [num for num in x if num != '.']
+            if len(set(temp)) != len(temp):
+                return False
+
+        for i in range(3):
+            for j in range(3):
+                tempAll = []
+                tempAll += [board[i * 3 + l][j * 3 + k] for k in range(3) for l in range(3)]
+                temp = [num for num in tempAll if num != '.']
+                print temp
+                if len(set(temp)) != len(temp):
+                    return False
+
+        return True
+
+s = Solution()
+print s.isValidSudoku([[".",".",".",".","5",".",".","1","."],[".","4",".","3",".",".",".",".","."],[".",".",".",".",".","3",".",".","1"],["8",".",".",".",".",".",".","2","."],[".",".","2",".","7",".",".",".","."],[".","1","5",".",".",".",".",".","."],[".",".",".",".",".","2",".",".","."],[".","2",".","9",".",".",".",".","."],[".",".","4",".",".",".",".",".","."]])
+# [".",".",".",".","5",".",".","1","."],
+# [".","4",".","3",".",".",".",".","."],
+# [".",".",".",".",".","3",".",".","1"],
+# ["8",".",".",".",".",".",".","2","."],
+# [".",".","2",".","7",".",".",".","."],
+# [".","1","5",".",".",".",".",".","."],
+# [".",".",".",".",".","2",".",".","."],
+# [".","2",".","9",".",".",".",".","."],
+# [".",".","4",".",".",".",".",".","."]
