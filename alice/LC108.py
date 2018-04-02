9f03218516a55236a825e48dd59be0e37185ef6c
alice <aliceadelice@gmail.com>
2018-04-02 11:31:10 -0700

    sorted array to BST: binary search to add left part as left subtree, same for right

---
+++ alice/LC108.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+
+        if not nums:
+            return None
+
+        left, right = 0, len(nums) - 1
+
+        mid = left + (right - left) / 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid + 1:])
+
+        return root
