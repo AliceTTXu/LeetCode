41c5571fd6e45b27a64462bd07d198325a500936
alice <aliceadelice@gmail.com>
2018-05-10 22:26:36 -0700

    FindPeak: binary search, if mid>mid+1, r = mid, elsel = mid + 1

---
+++ alice/LC162.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        if not nums:
+            return
+
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            mid = (l + r) / 2
+            if nums[mid] < nums[mid + 1]:
+                l = mid + 1
+            else:
+                r = mid
+
+        return l
